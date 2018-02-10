fa6bc05b64852c9ba3f751700f5cf2934241bc38
calebkloeckner <calebkloeckner@gmail.com>
2018-02-10 11:01:23 -0700

    added gitignore

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
